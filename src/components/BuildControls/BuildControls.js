f6b76117ef50203b19b05aa8fac58d9dba95fae7
Max-Yankulov <yankulov.max@gmail.com>
2019-03-01 21:33:57 +0200

    updated folder pathing and finishing with components

---
+++ src/components/BuildControls/BuildControls.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    <p>
+      Current Price: <strong>{props.price.toFixed(2)} $</strong>
+    </p>
+    {Object.keys(props.ingredients).map((ing) => {
+      const ingLabel = `${ing[0].toUpperCase()}${ing.slice(1)}`;
+      return (
+        <BuildControl
+          updateIngredientHandler={props.updateIngredientHandler}
+          label={ingLabel}
+          type={ing}
+          key={ingLabel}
+          isDisabled={props.ingredients[ing]}
+        />
+      );
+    })}
+  </div>
+);
+
+export default buildControls;
