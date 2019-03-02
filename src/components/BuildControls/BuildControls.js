2b97cdcd6fe84fe597c1d199a4da9194f7a41b82
Max-Yankulov <yankulov.max@gmail.com>
2019-03-02 18:24:17 +0200

    added button in buildControls comp and css

--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -20,6 +20,13 @@ const buildControls = (props) => (
         />
       );
     })}
+    <button
+      className={classes.OrderButton}
+      disabled={!props.purchasable}
+      onClick={props.purchased}
+    >
+      ORDER NOW
+    </button>
   </div>
 );
 
